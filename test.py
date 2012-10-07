20a61e8efd74e831cfaf74eda5438328479af9eb
arokem <arokem@gmail.com>
2012-10-07 14:44:44 -0700

    RF: Changed to my local configuration. Temporary...

--- test.py
+++ test.py
@@ -1,11 +1,15 @@
 from pymatbridge import Matlab
 
 # Initialise MATLAB
-mlab = Matlab(port=4000)
+mlab = Matlab(matlab='/Applications/MATLAB_R2010b.app/bin/matlab')
+
 # Start the server
 mlab.start()
 # Run a test function: just adds 1 to the argument a
+res = []
 for i in range(10):
-    print mlab.run('~/Sites/python-matlab-bridge/test.m', {'a': i})['result']
+     res.append(mlab.run('~/source/python-matlab-bridge/test.m', {'a': i})['result'])
+     print res[-1]
+
 # Stop the MATLAB server
 mlab.stop()
