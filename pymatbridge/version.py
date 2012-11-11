1cc7b80828aa4a228e02601bcbadb0b7988ad4c2
arokem <arokem@gmail.com>
2012-11-10 17:04:31 -0800

    ENH: Make this puppy py-install-able.

---
+++ pymatbridge/version.py
@@ -0,0 +1,90 @@
+"""pymatbridge version/release information"""
+
+# Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
+_version_major = 0
+_version_minor = 1
+_version_micro = ''  # use '' for first of series, number for 1 and above
+_version_extra = 'dev'
+#_version_extra = ''  # Uncomment this for full releases
+
+# Construct full version string from these.
+_ver = [_version_major, _version_minor]
+if _version_micro:
+    _ver.append(_version_micro)
+if _version_extra:
+    _ver.append(_version_extra)
+
+__version__ = '.'.join(map(str, _ver))
+
+CLASSIFIERS = ["Development Status :: 3 - Alpha",
+               "Environment :: Console",
+               "Intended Audience :: Science/Research",
+               "License :: OSI Approved :: BSD License",
+               "Operating System :: OS Independent",
+               "Programming Language :: Python",
+               "Topic :: Scientific/Engineering"]
+
+description = "pymatbridge is a set of python and matlab functions to allow these two systems to talk to each other"
+
+long_description = """
+
+Pymatbridge
+===========
+
+This package provides a set of python and matlab functions with the goal of
+providing an easy, seamless way to call Matlab functions within Python (not so
+much the other way, because why would anyone want to do something like that?).
+
+TODO: more documentation will be here at some point.
+
+License information
+===================
+
+Copyright (c) 2012, Max Jaderberg, Ariel Rokem 
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this
+list of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of the Oxford University, Stanford University nor the names of
+its contributors may be used to endorse or promote products derived from this
+software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS 'AS IS' AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+"""
+
+NAME = "pymatbridge"
+MAINTAINER = "Ariel Rokem"
+MAINTAINER_EMAIL = "arokem@gmail.com"
+DESCRIPTION = description
+LONG_DESCRIPTION = long_description
+URL = "https://github.com/arokem/python-matlab-bridge"
+DOWNLOAD_URL = "https://github.com/arokem/python-matlab-bridge/archive/master.tar.gz"
+LICENSE = "BSD"
+AUTHOR = "Max Jaderberg and Ariel Rokem"
+AUTHOR_EMAIL = "arokem@gmail.com"
+PLATFORMS = "OS Independent"
+MAJOR = _version_major
+MINOR = _version_minor
+MICRO = _version_micro
+VERSION = __version__
+PACKAGES = ['pymatbridge']
+PACKAGE_DATA = {}
+REQUIRES = []
---
+++ setup.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python
+"""Setup file for python-matlab-bridge"""
+
+import os
+import sys
+
+# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
+# update it when the contents of directories change.
+if os.path.exists('MANIFEST'):
+    os.remove('MANIFEST')
+
+from distutils.core import setup
+
+# Get version and release info, which is all stored in pymatbridge/version.py
+ver_file = os.path.join('pymatbridge', 'version.py')
+execfile(ver_file)
+
+opts = dict(name=NAME,
+            maintainer=MAINTAINER,
+            maintainer_email=MAINTAINER_EMAIL,
+            description=DESCRIPTION,
+            long_description=LONG_DESCRIPTION,
+            url=URL,
+            download_url=DOWNLOAD_URL,
+            license=LICENSE,
+            classifiers=CLASSIFIERS,
+            author=AUTHOR,
+            author_email=AUTHOR_EMAIL,
+            platforms=PLATFORMS,
+            version=VERSION,
+            packages=PACKAGES,
+            package_data=PACKAGE_DATA,
+            requires=REQUIRES,
+            )
+
+# For some commands, use setuptools.  Note that we do NOT list install here!
+# If you want a setuptools-enhanced install, just run 'setupegg.py install'
+needs_setuptools = set(('develop', ))
+if len(needs_setuptools.intersection(sys.argv)) > 0:
+    import setuptools
+
+# Only add setuptools-specific flags if the user called for setuptools, but
+# otherwise leave it alone
+if 'setuptools' in sys.modules:
+    opts['zip_safe'] = False
+
+# Now call the actual setup function
+if __name__ == '__main__':
+    setup(**opts)
