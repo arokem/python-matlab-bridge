40f932b873de74d6315910fbd8348b6fbc517c15
arokem <arokem@gmail.com>
2012-12-23 09:18:20 -0800

    Use more conventional call to tempname, instead of the 'randomly' generated file-name.
    I suspect that the rng is set to the same state at the beginning of each
    matlab session, so this would do exactly the same thing each time.

--- pymatbridge/matlab/private/multipart2struct.m
+++ pymatbridge/matlab/private/multipart2struct.m
@@ -36,8 +36,8 @@ switch(subdata.ContentType)
         subdata.ContentData=subrequestdata(w(i)+1:end);
         [pathstr,name,ext] = fileparts(subdata.Filename);
 
-				% [NULL,filename] = fileparts(tempname());  % more conventional;
-        filename = char(round(rand(1,32)*9)+48);
+        [NULL,filename] = fileparts(tempname());  % more conventional;
+        %filename = char(round(rand(1,32)*9)+48);
         fullfilename = fullfile(config.temp_folder,[filename ext]);
         fid = fopen(fullfilename,'w'); fwrite(fid,subdata.ContentData,'int8'); fclose(fid);
         subdata.Filename=fullfilename;
