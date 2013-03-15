39077d66c96f707c217e0065d96cc5e577e70002
arokem <arokem@gmail.com>
2013-03-15 11:14:40 -0700

    Increment version to tag + release.

--- pymatbridge/version.py
+++ pymatbridge/version.py
@@ -4,7 +4,7 @@
 _version_major = 0
 _version_minor = 1
 _version_micro = ''  # use '' for first of series, number for 1 and above
-_version_extra = 'dev'
+_version_extra = ''
 #_version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
