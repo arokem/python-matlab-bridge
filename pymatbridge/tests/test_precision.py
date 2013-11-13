6f0ba0e10810b31add40acbec45f8420ab859c4e
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-12 23:52:40 -0800

    Precision test skeleton added

---
+++ pymatbridge/tests/test_precision.py
@@ -0,0 +1,29 @@
+import pymatbridge as pymat
+import numpy.testing as npt
+import random as rd
+
+def test_number:
+    mlab = pymat.Matlab()
+    mlab.start()
+    npt.assert_(mlab.is_connected(), msg = "test_number: Connection failed")
+
+    a = 0
+    b = 0
+    for i in range(0, 20):
+        a = a/10 + rd.randint(0, 9)
+        b = b/10 + rd.randint(0, 9)
+        pythonSum = a + b
+        matlabSum = mlab.run_func('test_precision_sum.m',{'a':a, 'b', b})['result']
+        npt.assert_equal(pythonSum, matlabSum, err_msg = "test_number: Not equal")
+
+    mlab.stop()
+    npt.assert_(not mlab.is_connected(), msg = "test_number: Disconnection failed")
+
+def test_tuple:
+    pass
+
+def test_dict:
+    pass
+
+def test_array:
+    pass
