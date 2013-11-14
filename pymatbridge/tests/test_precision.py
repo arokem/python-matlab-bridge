4d95bd9189850e88e8aae2dcb8073c597e983086
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-14 11:57:59 -0800

    Added demo tests for dictionary type

--- pymatbridge/tests/test_precision.py
+++ pymatbridge/tests/test_precision.py
@@ -9,7 +9,7 @@ def test_number():
 
     a = 0
     b = 0
-    for i in range(0, 30):
+    for i in range(0, 20):
         a = a/10 + float(rd.randint(0, 9))
         b = b/10 + float(rd.randint(0, 9))
         pythonSum = a + b
@@ -23,7 +23,16 @@ def test_tuple():
     pass
 
 def test_dict():
-    pass
+    mlab = pymat.Matlab()
+    mlab.start()
+    npt.assert_(mlab.is_connected(), msg = "test_dict: Connection failed")
 
+    res = mlab.run_func("demo_dict.m",{'pDict':[{'apple':1.2,'pear':2.4,'banana':3.3}]})['result']
+    npt.assert_equal(res[0], 1.2, err_msg = "value1 not equal")
+    npt.assert_equal(res[1], 2.4, err_msg = "value2 not equal")
+    npt.assert_equal(res[2], 3,3, err_msg = "value3 not equal")
+
+    mlab.stop()
+    npt.assert_(not mlab.is_connected(), msg = "test_dict: Disconnection failed")
 def test_array():
     pass
