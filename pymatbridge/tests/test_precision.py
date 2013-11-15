41e0d90fa2dffb6b5b7678a31aea3583a84ba99d
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-14 16:56:08 -0800

    WIP. Added tests for float64 type summing

--- pymatbridge/tests/test_precision.py
+++ pymatbridge/tests/test_precision.py
@@ -22,7 +22,20 @@ def testFloat64Roundtrip():
     for i in range(0,20):
         val = np.float64(rd.random())
         res = mlab.run_func('test_precision_pass.m',{'val':val})['result']
-        npt.assert_almost_equal(res, val, decimal=8, err_msg="float64 roundtrip error")
+        npt.assert_almost_equal(res, val, decimal=4, err_msg="float64 roundtrip error")
+
+    stopMatlab(mlab)
+
+
+def testFloat64Sum():
+    mlab = connectToMatlab()
+
+    for i in range(0,20):
+        val1 = np.float64(rd.random())
+        val2 = np.float64(rd.random())
+
+        res = mlab.run_func('test_precision_sum.m',{'val1':val1,'val2':val2})['result']
+        npt.assert_almost_equal(res, val1 + val2, decimal=4, err_msg="float64 sum error")
 
     stopMatlab(mlab)
 
