734e4901ff43848b314ff20fbe48bc7de88704a7
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-17 13:35:43 -0800

    WIP added debug lines

--- pymatbridge/matlab/www/test_precision_pass.m
+++ pymatbridge/matlab/www/test_precision_pass.m
@@ -2,6 +2,7 @@ function res = test_precision_pass(args)
 % This function takes an argument val and simply return it
     
     format long;
-    res = args.val;
+    res = 2*args.val;
+    disp(args.val)
 
 end 
