023dd810e128a49105dc08da2dfca248510e42fd
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-17 12:24:31 -0800

    WIP. Added 'format long' into json2mat.m to prevent precision loss

--- pymatbridge/matlab/www/test_precision_sum.m
+++ pymatbridge/matlab/www/test_precision_sum.m
@@ -1,5 +1,6 @@
 function res = test_precision_sum(args)
 % This function returns the sum of two arguments val1 and val2
-	res = args.val1 + args.val2;
+	format long;
+    res = args.val1 + args.val2;
 
 end %function
