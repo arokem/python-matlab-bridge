0b3a979a5958c63d3f1b99d4f6fd20a337e207c0
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-17 12:16:39 -0800

    WIP

--- pymatbridge/matlab/www/test_precision_pass.m
+++ pymatbridge/matlab/www/test_precision_pass.m
@@ -1,7 +1,7 @@
 function res = test_precision_pass(args)
 % This function takes an argument val and simply return it
-    
-    sprintf('Input value is %f\n', args.val);
+   
+    format long;
     res = args.val;
 
 end 
