f6767d26876e53481cd97032f515a60ad6fc5fef
haoxingz <haoxing.zhang.david@gmail.com>
2013-11-19 11:24:27 -0800

    WIP

--- pymatbridge/matlab/webserver.m
+++ pymatbridge/matlab/webserver.m
@@ -238,9 +238,7 @@ try
 			case {'.m'}
 				fhandle = str2func(name);
 				try
-					html=feval(fhandle,request,config);
-				    disp('hey Im here!');
-                    disp(html);
+				%	html=feval(fhandle,request,config);
                 catch ME
 					html=['<html><body><font color="#FF0000">Error in file : ' name ...
 					'.m</font><br><br><font color="#990000"> The file returned the following error: <br>' ...
