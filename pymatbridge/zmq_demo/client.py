71a2260dfc4b9050262e271c59b15c3adc24a4cc
haoxingz <haoxing.zhang.david@gmail.com>
2013-12-22 13:42:59 -0800

    Evaluation of ZMQ in transfering large dataset over IPC

--- pymatbridge/zmq_demo/client.py
+++ pymatbridge/zmq_demo/client.py
@@ -1,1 +1,28 @@
-# This is the client
+import zmq, json, bson
+import numpy as np
+
+# Please uncomment the decorator when profiling
+# @profile
+def run_client():
+    # Initialize client
+    context = zmq.Context()
+    socket = context.socket(zmq.REQ)
+    socket.connect("ipc:///tmp/ipctest")
+
+    # Generate a 72MB random array
+    array = np.random.random_sample((3000,3000)).tolist()
+    argument = {'val':array}
+
+    # Send message and wait for a response
+    message_out = json.dumps(argument)
+    socket.send(message_out)
+    message_in = socket.recv()
+    result = json.loads(message_in)['res']
+
+    # Check if the received message matches the original one
+    if result == np.sum(array):
+        print "Success"
+        print "Sum of orriginal array: ", np.sum(array)
+        print "Sum calculated by the server: ", result
+
+run_client()
--- pymatbridge/zmq_demo/server.py
+++ pymatbridge/zmq_demo/server.py
@@ -1,1 +1,25 @@
-# This is the server
+import zmq, json
+import numpy as np
+
+def run_server():
+    # Initializing server
+    context = zmq.Context()
+    socket = context.socket(zmq.REP)
+    socket.bind("ipc:///tmp/ipctest")
+
+    while True:
+        message_in = socket.recv()
+        print "Request received"
+    # Retrieve array from the message
+        argument = json.loads(message_in)
+        array = argument['val']
+    # Calculate the sum of array elements
+        sum_array = np.sum(array)
+        result = {'res':sum_array}
+        message_out = json.dumps(result)
+    # Send out reply
+        socket.send(message_out)
+        print "Response sent"
+
+
+run_server()
