ff52e0827cb1a3988a411d173c8a87593d2a3719
haoxingz <haoxing.zhang.david@gmail.com>
2014-02-16 10:55:45 -0800

    Raise exception when path not found

--- pymatbridge/src/make.py
+++ pymatbridge/src/make.py
@@ -34,19 +34,19 @@ for line in config:
             matlab_path = find_path(path[1].split(','), 'matlab.exe')
         else:
             matlab_path = find_path(path[1].split(','), 'matlab')
+
         if matlab_path == "":
-            print "Cannot find Matlab. Please add the path to local.cfg"
-            success = False
-            break
+            raise ValueError("Could not find matlab. Please add its path to local.cfg")
+
         print "Done"
 
     elif path[0] == "HEADER_PATH":
         print "Searching for zmq.h ..."
         header_path = find_path(path[1].split(','), 'zmq.h')
+
         if header_path == "":
-            print "Cannot find zmq.h. Please add the path to local.cfg"
-            success = False
-            break
+            raise ValueError("Could not find zmq.h. Please add its path to local.cfg")
+
         print "Done"
 
     elif path[0] == "LIB_PATH":
@@ -62,40 +62,35 @@ for line in config:
             lib_path = find_path(path[1].split(','), 'libzmq.dll')
 
         if lib_path == "":
-            print "Cannot find libzmq. Please add the path to loacl.cfg"
-            success = False
-            break
+            raise ValueError("Could not find zmq library. Please add its path to local.cfg")
+
         print "Done"
 
 config.close()
 
-if success == False:
-    print "Building failed"
+# Get the extension
+if platform == 'win32':
+    extcmd = '"' + matlab_path + "\\mexext.bat" + '"'
+    check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
+    extension = check_extension.stdout.readline().rstrip('\r\n')
 else:
-    # Get the extension
-    if platform == 'win32':
-        extcmd = '"' + matlab_path + "\\mexext.bat" + '"'
-        check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
-        extension = check_extension.stdout.readline().rstrip('\r\n')
-    else:
-        extcmd = matlab_path + "/mexext"
-        check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
-        extension = check_extension.stdout.readline().rstrip('\r\n')
+    extcmd = matlab_path + "/mexext"
+    check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
+    extension = check_extension.stdout.readline().rstrip('\r\n')
 
-    # Build the mex file
-    if platform == 'win32':
-        mex = "\\mex.bat"
-    else:
-        mex = "/mex"
-    make_cmd = '"' + matlab_path + mex + '"' + " -O -I" + header_path + " -L" + lib_path + " -lzmq messenger.c"
-    os.system(make_cmd)
+# Build the mex file
+if platform == 'win32':
+    mex = "\\mex.bat"
+else:
+    mex = "/mex"
+make_cmd = '"' + matlab_path + mex + '"' + " -O -I" + header_path + " -L" + lib_path + " -lzmq messenger.c"
+os.system(make_cmd)
 
-    # Move to the ../matlab/ directory
-    if platform == 'win32':
-        move_cmd = "move messenger." + extension + " ..\\matlab\\"
-    else:
-        move_cmd = "mv messenger." + extension + " ../matlab/"
+# Move to the ../matlab/ directory
+if platform == 'win32':
+    move_cmd = "move messenger." + extension + " ..\\matlab\\"
+else:
+    move_cmd = "mv messenger." + extension + " ../matlab/"
 
-    os.system(move_cmd)
+os.system(move_cmd)
 
-    print "Building succeeded!"
