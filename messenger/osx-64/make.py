74d1d67ebca9631226152f40097a8f40ee636086
haoxingz <haoxing.zhang.david@gmail.com>
2014-03-17 16:02:13 -0700

    Refactored build code for osx-64 platform

--- messenger/osx-64/make.py
+++ messenger/osx-64/make.py
@@ -28,32 +28,22 @@ for line in config:
     path = line.split('=')
 
     if path[0] == "MATLAB_BIN":
-        matlab_path = path[1].rstrip('\r\n')
-        if matlab_path == "":
-            raise ValueError("Could not find Matlab bin directory. Please add it to MATLAB_BIN")
-        print "Matlab found in " + matlab_path
+        print "Searching for Matlab bin folder in local.cfg ..."
+        matlab_bin = path[1].rstrip('\r\n')
+        if matlab_bin == "":
+            raise ValueError("Could not find Matlab bin folder. Please add it to local.cfg")
+        print "Matlab found in " + matlab_bin
 
     elif path[0] == "HEADER_PATH":
-        print "Searching for zmq.h ..."
-        header_path = find_path(path[1].split(','), 'zmq.h')
-
+        print "Searching for zmq.h in local.cfg ..."
+        header_path = path[1].rstrip('\r\n')
         if header_path == "":
             raise ValueError("Could not find zmq.h. Please add its path to local.cfg")
-
         print "zmq.h found in " + header_path
 
     elif path[0] == "LIB_PATH":
-        # Dynamic library has different names on different platforms
-        if platform == 'darwin':
-            print "Searching for libzmq.dylib ..."
-            lib_path = find_path(path[1].split(','), 'libzmq.dylib')
-        elif platform == 'linux2':
-            print "Searching for libzmq.so ..."
-            lib_path = find_path(path[1].split(','), 'libzmq.so')
-        elif platform == 'win32':
-            print "Searching for libzmq.dll ..."
-            lib_path = find_path(path[1].split(','), 'libzmq.dll')
-
+        print "Searching for zmq library in local.cfg ..."
+        lib_path = path[1].rstrip('\r\n')
         if lib_path == "":
             raise ValueError("Could not find zmq library. Please add its path to local.cfg")
 
@@ -63,11 +53,11 @@ config.close()
 
 # Get the extension
 if platform == 'win32':
-    extcmd = '"' + matlab_path + "\\mexext.bat" + '"'
+    extcmd = '"' + matlab_bin + "\\mexext.bat" + '"'
     check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
     extension = check_extension.stdout.readline().rstrip('\r\n')
 else:
-    extcmd = matlab_path + "/mexext"
+    extcmd = matlab_bin + "/mexext"
     check_extension = subprocess.Popen(extcmd, stdout = subprocess.PIPE)
     extension = check_extension.stdout.readline().rstrip('\r\n')
 
@@ -78,16 +68,6 @@ if platform == 'win32':
     mex = "\\mex.bat"
 else:
     mex = "/mex"
-make_cmd = '"' + matlab_path + mex + '"' + " -O -I" + header_path + " -L" + lib_path + " -lzmq pymatbridge/src/messenger.c"
+make_cmd = '"' + matlab_bin + mex + '"' + " -O -I" + header_path + " -L" + lib_path + " -lzmq ../src/messenger.c"
 os.system(make_cmd)
-
-print "Moving messenger." + extension + " to pymatbridge/matlab/ ..."
-
-# Move to the ../matlab/ directory
-if platform == 'win32':
-    move_cmd = "move messenger." + extension + " pymatbridge\\matlab\\"
-else:
-    move_cmd = "mv messenger." + extension + " pymatbridge/matlab/"
-
-os.system(move_cmd)
 
