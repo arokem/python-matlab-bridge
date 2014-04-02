95e14f2602eaaa9c0ed5c48dbcae61b4902b97a2
arokem <arokem@gmail.com>
2014-04-02 11:48:27 -0700

    TST: Updating the tests to reflect RF of the publish code.

--- pymatbridge/tests/test_publish.py
+++ pymatbridge/tests/test_publish.py
@@ -7,18 +7,13 @@ def test_format_line():
 
     """
     npt.assert_equal(publish.format_line('%% This begins a new cell'),
-                      (True, True, ' This begins a new cell'))
+                      (True, True, ' This begins a new cell\n'))
 
     npt.assert_equal(publish.format_line('% This should be just markdown'),
-                      (False, True, ' This should be just markdown'))
+                      (False, True, ' This should be just markdown\n'))
 
     npt.assert_equal(publish.format_line('This is just code'),
                       (False, False, 'This is just code'))
-
-    # Special case: empty lines are interpreted as a signal to start a new
-    # cell:
-    npt.assert_equal(publish.format_line('\n'),
-                      (True, False, ''))
 
 
 def test_lines_to_notebook():
@@ -35,5 +30,5 @@ def test_lines_to_notebook():
 
     nb = publish.lines_to_notebook(lines)
 
-    npt.assert_equal(nb['worksheets'][0]['cells'][0]['source'],
-                     ' This is a first line\n')
+    npt.assert_equal(nb['worksheets'][0]['cells'][0]['source'][0],
+                     ' This is a first line\n\n')
