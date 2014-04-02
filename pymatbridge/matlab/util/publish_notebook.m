8ad3e024d544c79329eb8411d173763c6a3f4b49
arokem <arokem@gmail.com>
2014-04-02 15:44:37 -0700

    RF: Point to the right bin directory for notebook conversion.

--- pymatbridge/matlab/util/publish_notebook.m
+++ pymatbridge/matlab/util/publish_notebook.m
@@ -18,9 +18,21 @@ end
 
 % Matlab system path-setting is broken, so we place the
 % publish-notebook executable right here, so we can easily find it
-binpath = fileparts(which(mfilename));
-pkgpath = fileparts(fileparts(fileparts(binpath)));
-cmd_str = sprintf('%s/publish-notebook %s %s', binpath, mfile, outputfile);
+libpath = fileparts(which(mfilename));
+prefix = strsplit(libpath, 'lib');
+binpath = strcat(prefix(1), 'bin');
+cmd_str = sprintf('publish-notebook %s %s', mfile, outputfile);
 
-setenv('PYTHONPATH', pkgpath);
-[status,result] = system(cmd_str)
+setenv('PATH', sprintf('%s:%s', binpath{1}, getenv('PATH')));
+[status, result] = system(cmd_str); 
+
+if status == 0 
+    disp('Conversion completed');
+else
+    disp(sprintf('There was a problem converting the file %s:', mfile));
+    disp(result)
+end
+
+
+
+
