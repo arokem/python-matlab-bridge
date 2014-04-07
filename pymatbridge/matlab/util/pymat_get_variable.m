12e0411ed0d0300ed359d98ce1fa60552f2503d9
Hilton Bristow <hilton.bristow@gmail.com>
2014-04-07 19:12:30 +1000

    Initial pythonic API teaser

--- pymatbridge/matlab/util/pymat_get_variable.m
+++ pymatbridge/matlab/util/pymat_get_variable.m
@@ -2,31 +2,16 @@ function json_response = pymat_get_variable(req)
 % Reach into the current namespace get a variable in json format that can
 % be returned as part of a response
 
-response.success = 'false';
-
-field_names = fieldnames(req);
-
-response.content = '';
-
-varname_check = false;
-if size(field_names)
-    if isfield(req, 'varname')
-        varname_check = true;
+    try
+        response.result  = evalin('base', req.varname);
+        response.success = true;
+    catch exception
+        response.success = false;
+        response.result  = exception.identifier;
+        response.message = exception.message;
     end
-end
 
-if ~varname_check
-    response.message = 'No variable name provided as input argument';
     json_response = json.dump(response);
     return
-end
 
-
-varname = req.varname;
-
-response.var = evalin('base', varname);
-
-json_response = json.dump(response);
-
-return
 end
