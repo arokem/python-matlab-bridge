07acc4eea413c9e11acbcc79a4e5a6479d45941d
Hilton Bristow <hilton.bristow@gmail.com>
2014-04-10 16:21:43 +1000

    Migrated setup.py to setuptools. Merged version.py into setup.py. Bumped minor version number to signify new API

--- setup.py
+++ setup.py
@@ -1,67 +1,91 @@
 #!/usr/bin/env python
-"""Setup file for python-matlab-bridge"""
-
 import os
 import sys
+import glob
 import shutil
-
-# BEFORE importing distutils, remove MANIFEST. distutils doesn't properly
-# update it when the contents of directories change.
-if os.path.exists('MANIFEST'):
-    os.remove('MANIFEST')
-
-from distutils.core import setup
-
-# Find the messenger binary file and copy it to /matlab folder.
-
-def copy_bin(bin_path):
-    if os.path.exists(bin_path):
-        shutil.copy(bin_path, "./pymatbridge/matlab")
-        return True
-    else:
-        return False
-
-if sys.platform == "darwin":
-    if not copy_bin("./messenger/mexmaci64/messenger.mexmaci64"):
-        raise ValueError("messenger.mexmaci64 is not built yet. Please build it yourself.")
-
-elif sys.platform == "linux2":
-    if not copy_bin("./messenger/mexa64/messenger.mexa64"):
-        raise ValueError("messenger.mexa64 is not built yet. Please build it yourself.")
-
-elif sys.platform == "win32":
-    t1 = copy_bin("./messenger/mexw64/messenger.mexw64")
-    t2 = copy_bin("./messenger/mexw32/messenger.mexw32")
-    if not (t1 or t2):
-        raise ValueError("Neither messenger.mexw32 or mex264 is built yet. Please build the appropriate one yourself") 
-
-else:
-    raise ValueError("Known platform")
-
-# Get version and release info, which is all stored in pymatbridge/version.py
-ver_file = os.path.join('pymatbridge', 'version.py')
-exec(open(ver_file).read())
-
-opts = dict(name=NAME,
-            maintainer=MAINTAINER,
-            maintainer_email=MAINTAINER_EMAIL,
-            description=DESCRIPTION,
-            long_description=LONG_DESCRIPTION,
-            url=URL,
-            download_url=DOWNLOAD_URL,
-            license=LICENSE,
-            classifiers=CLASSIFIERS,
-            author=AUTHOR,
-            author_email=AUTHOR_EMAIL,
-            platforms=PLATFORMS,
-            version=VERSION,
-            packages=PACKAGES,
-            package_data=PACKAGE_DATA,
-            requires=REQUIRES,
-            scripts=BIN
-            )
+from setuptools import setup
 
 
-# Now call the actual setup function
-if __name__ == '__main__':
-    setup(**opts)
+# ----------------------------------------------------------------------------
+# HELPERS
+# ----------------------------------------------------------------------------
+def read(file_name):
+  with open(os.path.join(os.path.dirname(__file__), file_name)) as f:
+    return f.read()
+
+
+# ----------------------------------------------------------------------------
+# MEX MESSENGER
+# ----------------------------------------------------------------------------
+messengers = {
+    'darwin': ['messenger/mexmaci64/messenger.mexmaci64'],
+    'linux2': ['messenger/mexa64/messenger.mexa64'],
+    'win32':  ['messenger/mexw64/messenger.mexw64',
+               'messenger/mexw32/messenger.mexw32']
+}.get(sys.platform, [])
+
+for messenger in messengers:
+    try:
+        shutil.copy(messenger, 'pymatbridge/matlab')
+    except IOError:
+        pass
+
+if not glob.glob('pymatbridge/matlab/messenger.mex*'):
+    raise IOError('The Matlab messenger mex script is not yet built for your system. '
+                  'Please build messenger/src/messenger.c manually and copy the '
+                  'binary to pymatbridge/matlab before continuing with setup.')
+
+
+# ----------------------------------------------------------------------------
+# SETUP
+# ----------------------------------------------------------------------------
+__version__ = '0.4.dev'
+
+setup(
+    name = 'pymatbridge',
+    version = __version__,
+    platforms = 'OS Independent',
+    description = 'A package to call Matlab functions from Python',
+    long_description = read('README.md'),
+    maintainer = 'Ariel Rokem',
+    maintainer_email = 'arokem@gmail.com',
+    url = 'https://github.com/arokem/python-matlab-bridge',
+    download_url = 'https://github.com/arokem/python-matlab-bridge/archive/master.tar.gz',
+    license = 'BSD',
+    packages = [
+        'pymatbridge'
+    ],
+    install_requires = [
+        'numpy>=1.7.0',
+        'pyzmq>=13.0.0'
+    ],
+    entry_points = {
+        'console_scripts': [
+            'publish-notebook = pymatbridge.publish:main'
+        ]
+    },
+    package_data = {
+        'pymatbridge': [
+            'matlab/matlabserver.m',
+            'matlab/messenger.*',
+            'matlab/usrprog/*',
+            'matlab/util/*.m',
+            'matlab/util/json_v0.2.2/LICENSE',
+            'matlab/util/json_v0.2.2/README.md',
+            'matlab/util/json_v0.2.2/test/*',
+            'matlab/util/json_v0.2.2/+json/*.m',
+            'matlab/util/json_v0.2.2/+json/java/*',
+            'tests/*.py',
+            'examples/*.ipynb'
+        ]
+    },
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Scientific/Engineering'
+    ]
+)
