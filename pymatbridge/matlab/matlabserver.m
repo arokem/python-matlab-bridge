7d38469b00f3eb32d1251e9586bf73e7cd85b589
John Stowers <john.stowers@gmail.com>
2014-07-08 18:56:00 +0200

    Pass and return args as dictionaries
    Due to the inherent ambiguity in matlab's JSON
    decoding ability this is the only way to make
    passing >1 same-sized array arguments work. Otherwise
    the matlab json library coerces them to a matrix

--- pymatbridge/matlab/matlabserver.m
+++ pymatbridge/matlab/matlabserver.m
@@ -62,9 +62,11 @@ function resp = call(req)
 %   'args': A cell array of args to expand into the function arguments
 %   'nout': The number of output arguments requested
 
-    % function of no arguments
-    if ~isfield(req, 'args')
-        req.args = {}
+    args = {};
+    if req.nin > 0
+        for i=1:req.nin
+            args{i} = req.args.(['a' num2str(i-1)]);
+        end
     end
 
     % determine the number of output arguments
@@ -83,22 +85,33 @@ function resp = call(req)
     % call the function, taking care of broadcasting outputs
     switch nout
         case 0
-            func(req.args{:});
+            func(args{:});
         case 1
-            resp.result = func(req.args{:});
+            resp.result = func(args{:});
         otherwise
-            [resp.result{1:nout}] = func(req.args{:});
+            [resp.result{1:nout}] = func(args{:});
+            if ~length(nsaveout)
+                %because of ambiguity of json encoding arrays of matrices
+                %convert multiple output arguments into a structure with
+                %fields name a0..aN. Convert these back to a list of matrices
+                %at the python end
+                result_struct = struct('nout',nout);
+                for i=1:nout
+                    result_struct.(['a' num2str(i-1)]) = resp.result{i};
+                end
+                resp.result = result_struct;
+            end
     end
 
     if length(nsaveout)
         if nout == 1
             assignin('base',saveout{1},resp.result);
-            resp.result = ['__VAR=' saveout{1} '|' class(resp.result)];
+            resp.result = ['__VAR=' saveout{1} '|' class(resp.result) '(' mat2str(size(resp.result)) ')'];
         elseif nout > 1
             tmp_result = '';
             for i=1:nout
                 assignin('base',saveout{i},resp.result{i});
-                tmp_result = ['__VAR=' saveout{i} '|' class(resp.result{i}) ';' tmp_result];
+                tmp_result = ['__VAR=' saveout{i} '|' class(resp.result{i}) '(' mat2str(size(resp.result{i})) ');' tmp_result];
             end
             resp.result = tmp_result;
         end
