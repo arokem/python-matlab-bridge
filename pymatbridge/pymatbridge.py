e24e957d63b2c6d56ae42101207b5417ac61d0fd
Steven Silvester <steven.silvester@ieee.org>
2014-10-31 13:45:05 -0500

    Add descriptive comment

--- pymatbridge/pymatbridge.py
+++ pymatbridge/pymatbridge.py
@@ -115,6 +115,9 @@ class _Session(object):
         raise NotImplemented
 
     def _preamble_code(self):
+        # suppress warnings while loading the path, in the case of
+        # overshadowing a built-in function on a newer version of
+        # Matlab (e.g. isrow)
         return ["warning('off','all')",
                 "addpath(genpath('%s'))" % MATLAB_FOLDER,
                 "warning('on', 'all')"]
