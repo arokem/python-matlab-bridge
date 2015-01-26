d7a07739ac65da751fb335dc9fcbaa4a351cb082
arokem <arokem@gmail.com>
2015-01-25 21:09:19 -0800

    Moving ahead?

--- pymatbridge/version.py
+++ pymatbridge/version.py
@@ -2,10 +2,10 @@
 
 # Format expected by setup.py and doc/source/conf.py: string of form "X.Y.Z"
 _version_major = 0
-_version_minor = 4
-_version_micro = 3#''  # use '' for first of series, number for 1 and above
-    #_version_extra = dev'
-_version_extra = ''  # Uncomment this for full releases
+_version_minor = 5
+_version_micro = 0 #''  # use '' for first of series, number for 1 and above
+_version_extra = dev
+#_version_extra = ''  # Uncomment this for full releases
 
 # Construct full version string from these.
 _ver = [_version_major, _version_minor]
