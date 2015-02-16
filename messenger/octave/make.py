5ae3d2a982091851d83182d97c28ad92631af0da
Steven Silvester <steven.silvester@ieee.org>
2015-02-15 22:03:58 -0600

    Remove old octave dir

--- messenger/octave/make.py
+++
@@ -1,7 +0,0 @@
-
-import os
-
-os.system('sudo apt-get install libzmq3-dev liboctave-dev')
-os.system('cd ../src; mkoctfile --mex -lzmq messenger.c')
-os.system('mv ../src/messenger.mex .')
-os.system('cp messenger.mex ../pymatbridge/matlab')
