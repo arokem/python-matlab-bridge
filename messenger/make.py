a3e26df3a8fc97e837473c5546efd0d2a9320830
Steven Silvester <steven.silvester@ieee.org>
2015-02-16 07:41:50 -0600

    Clean up install and configs

--- messenger/make.py
+++ messenger/make.py
@@ -1,26 +1,11 @@
 #!/usr/bin/python
 from __future__ import print_function
 import os
-import sys
-import fnmatch
+from get_messenger_dir import get_messenger_dir
 import subprocess
 import shutil
 
-# Check the system platform first
-platform = sys.platform
-print("This is a " + platform + " system")
-
-if platform.startswith('linux'):
-    messenger_dir = 'mexa64'
-elif platform.startswith('darwin'):
-    messenger_dir = 'mexmaci64'
-elif platform.startswith('win32'):
-    # We further need to differniate 32 from 64 bit:
-    maxint = sys.maxint()
-    if maxint == 9223372036854775807:
-        messenger_dir = 'mexw64'
-    elif maxint == 2147483647:
-        messenger_dir = 'mexw32'
+messenger_dir = get_messenger_dir()
     
 # Open the configure file and start parsing
 with open(os.path.join(messenger_dir, 'local.cfg'), 'r') as config:
--- messenger/make_octave.py
+++ messenger/make_octave.py
@@ -1,25 +1,11 @@
 #!/usr/bin/python
 from __future__ import print_function
 import os
-import sys
+from get_messenger_dir import get_messenger_dir
 import shutil
 import subprocess
 
-# Check the system platform first
-platform = sys.platform
-print("This is a " + platform + " system")
-
-if platform.startswith('linux'):
-    messenger_dir = 'mexa64'
-elif platform.startswith('darwin'):
-    messenger_dir = 'mexmaci64'
-elif platform.startswith('win32'):
-    # We further need to differniate 32 from 64 bit:
-    maxint = sys.maxsize
-    if maxint == 9223372036854775807:
-        messenger_dir = 'mexw64'
-    elif maxint == 2147483647:
-        messenger_dir = 'mexw32'
+messenger_dir = get_messenger_dir()
 
 with open(os.path.join(messenger_dir, 'local_octave.cfg')) as fid:
     lines = fid.readlines()
@@ -27,16 +13,16 @@ with open(os.path.join(messenger_dir, 'local_octave.cfg')) as fid:
 cfg = {}
 for line in lines:
     name, path = line.split('=')
-    cfg[name.lower()] = path
+    cfg[name.lower()] = path.strip() or '.'
 
-print("Building messenger.oct...")
+print("Building messenger.mex...")
 
 paths = "-L%(octave_lib)s -I%(octave_inc)s -L%(zmq_lib)s -I%(zmq_inc)s" % cfg
-make_cmd = "mkoctfile %s -lzmq ./src/messenger.c" % paths
+make_cmd = "mkoctfile --mex %s -lzmq ./src/messenger.c" % paths
 print(make_cmd)
 subprocess.check_output(make_cmd.split())
 
-messenger_exe = 'messenger.oct'
+messenger_exe = 'messenger.mex'
 messenger_loc = os.path.join(messenger_dir, messenger_exe)
 
 shutil.move(messenger_exe, messenger_loc)
