723b1ac6723caf42458288cd39b5aa091c4c1c15
AdnanSE <72250612+AdnanSE@users.noreply.github.com>
2020-10-02 13:40:13 +0500

    type added

---
+++ type.py
@@ -0,0 +1,20 @@
+print(type(5))
+print(type(10))
+
+print(type(3.8))
+print(type(5.0))
+
+print(type("computer"))
+print(type("laptop"))
+
+print(type(5) == type(10))
+print(type("computer") == type("laptop"))
+print(type(5) == type(5.0))
+print(type(5) == type("5"))
+
+print(type(True))
+print(type(False))
+print(type(True) == type(False))
+
+print(type([1, 2, 3]))
+print(type({ "NJ": "Trenton" }))
